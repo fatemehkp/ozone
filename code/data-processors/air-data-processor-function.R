6f67e588bf968d4d3d4981a3f17ac4f8554fedec
Fatemeh Kazemi <fkazem01@tufts.edu>
2021-01-16 13:55:20 -0500

    download, clean, cross-walk ozone data ready to merge with cms

---
+++ code/data-processors/air-data-processor-function.R
@@ -0,0 +1,52 @@
+### Author: Fatemeh Kazemi - 01-01-2021
+
+library(tidyverse)
+library(lubridate)
+
+# Air_Hourly_Process function
+### Function *** Air_Hourly_Process ***
+### Processes Hourly data 
+### Chooses a subset of monitors on the basis of: 
+### ... A minimum for number of :
+### ... Hourly measurements per Day,
+### ... Daily measurements per Month,
+### ... Months with minimum daily measurements per Year
+### ... Years with minimum Month and Day
+
+# Function Arguments: 
+# m1 : start month ( m1= 4(warm season) & 1(whole year))
+# m2 : end month   ( m2= 9(warm season) & 12(whole year))
+# h : minimum number of hours per day
+# d : minimum number of days per month
+# m : minimum numbers of month per year
+# y : minimum numbers of years per period
+# Packages: tidyverse; data.table
+
+Air.Hourly.Process <- function(dt, m1, m2, h, d, m, y) {
+  # identify qualified monitors and merged it with dt (hourly dataset)
+  dt.c <- dt %>%
+    mutate(Year = year(Date),
+           Month = month(Date),
+           Day = day(Date)) %>% 
+    filter(between(Month, m1, m2)) %>% 
+    group_by(Monitor.ID, Year, Month, Day) %>%
+    summarize(n_Hour = length(unique(Time))) %>%
+    filter(n_Hour >= h
+    ) %>% #monitors wt at least -h- hours each day
+    group_by(Monitor.ID, Year, Month) %>%
+    summarize(n_Day = length(unique(Day))) %>%
+    filter(n_Day >= d
+    ) %>% #monitors wt at least -d- days each month 
+    group_by(Monitor.ID, Year) %>%
+    summarize(n_Month = length(unique(Month))) %>%
+    filter(n_Month >= m
+    ) %>% #monitors wt at least -m- month each year
+    group_by(Monitor.ID) %>%
+    summarize(n_Year = length(unique(Year))) %>%
+    filter(n_Year >= y
+    ) %>% #monitors wt at least -y- year in start-end period
+    merge(dt
+    ) %>% 
+    select(-n_Year)
+  return(dt.c)
+}
---
+++ code/data-processors/epa-air-data-download-function.R
@@ -0,0 +1,106 @@
+### Author: Fatemeh Kazemi - 01-15-2021
+
+# Function that Downloads and cleans air quality data measured by EPA 
+# https://aqs.epa.gov/aqsweb/airdata/download_files.html
+
+# EPA.AQS.download.h Function
+# Function Arguments: 
+# period : hourly
+# Index : list of file index to download
+# start : start year 
+# end : end year
+# Packages: tidyverse, lubridate
+
+library(tidyverse)
+library(lubridate)
+
+EPA.AQS.download.h <- function(period, Index, start, end) {
+  
+  dt = data.frame()
+  for (i in Index) {
+    for (t in start:end) {
+      temp <- tempfile()
+      download.file(paste("https://aqs.epa.gov/aqsweb/airdata/",period,"_",i,"_",t
+                          ,".zip",sep = ""),temp)
+      dt0 <- read.csv(unz(temp, paste(period,"_",i,"_",t,".csv", sep = ""))) %>% 
+        mutate(State.Code = as.factor(State.Code))
+      dt <- rbind(dt, dt0)
+    }
+  }
+  
+  dt <- dt %>% 
+    filter(!State.Name %in% c("Alaska","Hawaii","Puerto Rico","Virgin Islands",
+                              "Country Of Mexico","Canada") #remove nonCONUS
+    ) %>%
+    transmute(
+      #unique ID for each Site: ss-ccc-nnnn
+      Site.ID = as.factor(sprintf("%02d-%03d-%04d",
+                                  as.numeric(as.character(State.Code)),
+                                  County.Code, Site.Num)),
+      POC,
+      #unique ID for each Monitor: ss-ccc-nnnn-pp
+      Monitor.ID = as.factor(sprintf("%02d-%03d-%04d-%02d",
+                                     as.numeric(as.character(State.Code)),
+                                     County.Code, Site.Num, POC)),
+      Parameter.Code,
+      Parameter.Name,
+      Date = ymd(Date.Local),
+      Time = Time.Local,
+      Sample.Measurement
+    ) %>%
+    select(Site.ID, POC, Monitor.ID, Parameter.Code, Parameter.Name, Date, Time,
+           Sample.Measurement) %>% 
+    arrange(Site.ID, POC, Date, Time) 
+  return(dt)
+}
+
+
+
+
+# EPA.AQS.download.8h Function
+# Function Arguments: 
+# period : 8hour
+# Index : list of file index to download
+# start : start year 
+# end : end year
+# Packages: tidyverse, lubridate
+
+EPA.AQS.download.8h <- function(period, Index, start, end) {
+  
+  dt = data.frame()
+  for (i in Index) {
+    for (t in start:end) {
+      temp <- tempfile()
+      download.file(paste("https://aqs.epa.gov/aqsweb/airdata/",period,"_",i,"_",t
+                          ,".zip",sep = ""),temp)
+      dt0 <- read.csv(unz(temp, paste(period,"_",i,"_",t,".csv", sep = ""))) %>% 
+        mutate(State.Code = as.factor(State.Code))
+      dt <- rbind(dt, dt0)
+    }
+  }
+  
+  dt <- dt %>% 
+    filter(!State.Code %in% c("02","15","72","78","80","CC") #remove nonCONUS
+    ) %>%
+    transmute(
+      #unique ID for each Site: ss-ccc-nnnn
+      Site.ID = as.factor(sprintf("%02d-%03d-%04d",
+                                  as.numeric(as.character(State.Code)),
+                                  County.Code, Site.Num)),
+      POC,
+      #unique ID for each Monitor: ss-ccc-nnnn-pp
+      Monitor.ID = as.factor(sprintf("%02d-%03d-%04d-%02d",
+                                     as.numeric(as.character(State.Code)),
+                                     County.Code, Site.Num, POC)),
+      Parameter.Code,
+      Parameter.Name,
+      Date = ymd(Date.Local),
+      Time = Time.Local,
+      Mean.Including.All.Data
+    ) %>%
+    select(Site.ID, POC, Monitor.ID, Parameter.Code, Parameter.Name, Date, Time,
+           Mean.Including.All.Data) %>% 
+    arrange(Site.ID, POC, Date, Time) 
+  return(dt)
+}
+
